f88aebe6b788b0f39b247b3aa1061facbb419aab
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2019-11-10 15:38:08 +0800

    feat: eslint plugin

---
+++ packages/eslint/src/index.ts
@@ -0,0 +1,61 @@
+import { Formatter } from "regularjs-beautify-core";
+import { Rule } from "eslint";
+import { Node } from "estree";
+
+const meta = {
+  type: "layout" as any,
+  docs: {
+    description: "beautify regularjs",
+    category: "Stylistic Issues",
+    recommended: true,
+    url: "https://github.com/hsiaosiyuan0/regularjs-beautify"
+  },
+  fixable: "code" as any
+};
+
+const create = (ctx: Rule.RuleContext) => {
+  const printWidth = ctx.options[0] || 80;
+
+  return {
+    TemplateLiteral(node: Node) {
+      const str = ctx.getSourceCode().getText(node);
+      const code = str.slice(1, -1);
+      const tag = code.match(/\s*<!--\s*@regularjs\s*-->/);
+      if (tag === null) return;
+
+      let beginLine = node.loc.start.line;
+      const precedingLines = str.slice(0, tag.index).split(/\r?\n/).length;
+      beginLine += precedingLines;
+
+      const space = tag[0].match(/^\s*/);
+      const baseIndent = space && space[0] ? space[0].length & ~1 : 2;
+      const formatter = new Formatter(code, ctx.getFilename(), beginLine, {
+        baseIndent,
+        printWidth
+      });
+      try {
+        const formatted = formatter.run();
+        const output = `\n${formatted}`;
+        if (output === code) return;
+        ctx.report({
+          node: node,
+          message: "poor style used in template",
+          fix(fixer: Rule.RuleFixer) {
+            return fixer.replaceTextRange(node.range, `\`${output}\``);
+          }
+        });
+      } catch (e) {}
+    }
+  };
+};
+
+export const regularjs: Rule.RuleModule = {
+  meta,
+  create
+};
+
+export default {
+  rules: {
+    regularjs
+  }
+};
---
+++ packages/eslint/tests/init-test.ts
@@ -0,0 +1,21 @@
+import "mocha";
+import { expect } from "chai";
+import { Linter } from "eslint";
+import plugin from "../src/index";
+
+describe("init test", () => {
+  it("should return true", () => {
+    const linter = new Linter();
+    linter.defineRules(plugin.rules);
+    const results = linter.verifyAndFix(
+      `const tpl = \`
+        <!--@regularjs-->
+    <div>   </div>\`;`,
+      {
+        parserOptions: { ecmaVersion: 2015 },
+        rules: { regularjs: ["error", 80] }
+      }
+    );
+    expect(results.messages.length).to.equal(0);
+  });
+});
